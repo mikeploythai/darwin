a5d2b9000af05bb7a8e155cb52c0e4984aaa545a
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2025-09-04 12:43:15 -0700

    refactor: remove manual auth header check
    verifySignatureAppRouter already verifies requests for me

--- src/app/send/route.ts
+++ src/app/send/route.ts
@@ -1,7 +1,7 @@
 import { DiscordAPIError, REST } from "@discordjs/rest";
 import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs";
 import { Routes } from "discord-api-types/v10";
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 if (!BOT_TOKEN) throw new Error("BOT_TOKEN is missing");
@@ -11,28 +11,19 @@ if (!CHANNEL_ID) throw new Error("CHANNEL_ID is missing");
 
 const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
 
-export const POST = verifySignatureAppRouter(
-  async ({ headers }: NextRequest) => {
-    const authHeader = headers.get("authorization");
-    if (authHeader !== `Bearer ${process.env.QSTASH_TOKEN}`) {
-      return new Response("Unauthorized", {
-        status: 401,
-      });
-    }
-
-    try {
-      await rest.post(Routes.channelMessages(CHANNEL_ID), {
-        body: {
-          content: "A message via REST!",
-        },
-      });
-      return new Response("Message sent!");
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json(
-        { error },
-        { status: error instanceof DiscordAPIError ? error.status : 400 },
-      );
-    }
-  },
-);
+export const POST = verifySignatureAppRouter(async () => {
+  try {
+    await rest.post(Routes.channelMessages(CHANNEL_ID), {
+      body: {
+        content: "A message via REST!",
+      },
+    });
+    return new Response("Message sent!");
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error },
+      { status: error instanceof DiscordAPIError ? error.status : 400 },
+    );
+  }
+});
